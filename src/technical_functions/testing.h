b9e3688528575620ffb7debaea1beb43b57f81f6
Rombel Stielz <rombel@whitehouse,gov>
2024-10-12 23:31:45 +0200

    Testing with macros, started with opening_book
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

---
+++ src/technical_functions/testing.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+#define CTEST std::cerr
+
+#define ACCUMULATE(pointer_to_bool, additional_condition) pointer_to_bool &= (additional_condition)
+ 
+#define EXPECT(condition) \
+if ((condition)) { \
+    CTEST << "[OK] " << #condition << std::endl; \
+} else { \
+    CTEST << "[ERROR] " << #condition << std::endl; \
+    return false; \
+}
+ 
+#define SILENT_EXPECT(condition) \
+if ((condition) == false) { \
+    CTEST << "[ERROR] " << #condition << std::endl; \
+    return false; \
+}
+
+#define BEGIN_TESTSUITE(name) CTEST << "TESTSUITE " << (name) << std::endl;
+   
