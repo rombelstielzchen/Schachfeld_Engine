8b379508f65084ec25bfa97e32703afecbe78940
Rombel Stielz <rombel@whitehouse,gov>
2024-11-22 18:52:09 +0100

    move_maker: test_case for undie_promotion (red)
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/board/test_move_maker.cpp
+++ src/board/test_move_maker.cpp
@@ -34,7 +34,6 @@ bool CTestMoveMaker::test_make_unmake_combinations(const std::string &position) 
    for (int j = 0; j < n_moves; ++j) {
         SMove move = move_generator.move_list.get_next();
         CTEST << "Make / unmake " << move_as_text(move) << "...\n";
-        // TODO: get_next
         board.move_maker.make_move(move);
         board.move_maker.unmake_move();
         SILENT_EXPECT(board.get_fen_position() == position);
@@ -45,15 +44,14 @@ bool CTestMoveMaker::test_make_unmake_combinations(const std::string &position) 
 bool CTestMoveMaker::test_algebraic_game() {
     CTEST << "CTestMoveMaker::test_algebraic_game() ...\n";
     CTEST << "Move-history: " <<board.move_maker.move_history.size() << "\n";
-    board.set_fen_position("startpos moves g1f3 d7d5 g2g3 c7c6 f1g2 g8f6 e1g1 e7e6 a2a4 f8e7 a4a5 b7b5 a5b6 e8g8 b6b7 b8d7");// b7a8b");
+    board.set_fen_position("startpos moves g1f3 d7d5 g2g3 c7c6 f1g2 g8f6 e1g1 e7e6 a2a4 f8e7 a4a5 b7b5 a5b6 e8g8 b6b7 b8d7 b7a8b");
     CTEST << "Move-history: " <<board.move_maker.move_history.size() << "\n";
     EXPECT(board.get_square(FILE_G, RANK_1) == WHITE_KING);
     EXPECT(board.get_square(FILE_F, RANK_1) == WHITE_ROOK);
     EXPECT(board.square_is_empty(FILE_E, RANK_1));
     EXPECT(board.square_is_empty(FILE_H, RANK_1));
     EXPECT(board.square_is_empty(FILE_B, RANK_5));
-// TODO    
-//    EXPECT(board.get_square(FILE_A, RANK_8) == WHITE_BISHOP);
+    EXPECT(board.get_square(FILE_A, RANK_8) == WHITE_BISHOP);
    CTEST << "Now unmaking all moves ...\n";
    board.move_maker.unmake_all();
    EXPECT(board.get_fen_position() == START_POSITION);
