aa9560d71bc0bc7559ded8180923625b64ebca1c
Rombel Stielz <rombel@whitehouse,gov>
2024-11-25 20:34:02 +0100

    stubs and makefile
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

---
+++ src/search/iterative_deepening.cpp
@@ -0,0 +1,7 @@
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+#include "iterative_deepening.h"
+
---
+++ src/search/iterative_deepening.h
@@ -0,0 +1,12 @@
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+class CIterativeDeepening {
+  public:
+    CIterativeDeepening();
+  public:
+    void search();
+};
+
