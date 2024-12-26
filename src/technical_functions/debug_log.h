86ad1e40271f7ebf6e4f529330c36c5ce4ee85f4
Rombel Stielz <rombel@whitehouse,gov>
2024-12-26 22:26:47 +0100

    debug_log heavily rewritten
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/technical_functions/debug_log.h
+++ src/technical_functions/debug_log.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Code based om the logging-framework by Fredrik Bornander
+// Code based on the logging-framework by Fredrik Bornander
 // https://www.codeproject.com/Articles/63736/Simple-debug-log-for-C
 // CodeProject license
 // 
@@ -31,43 +31,62 @@
 #include <string>
 #include <time.h>    
 
-#define DEBUG_SET_STREAM(stream) {  CLog::set_stream(stream); }
-#define DEBUG_LOG_TO_FILE() { CLog::log_to_file(); }
+#define DEBUG_SET_STREAM(stream) {  set_stream(stream); }
+#define DEBUG_LOG_TO_FILE() { log_to_file(); }
 #define DEBUG_METHOD() CLog _debugLog(__FUNCTION__);
-#define DEBUG_MESSAGE(debug_message) { _debugLog.message(debug_message); }
-#define DEBUG_VALUE_OF(variable) { _debugLog.value_of(#variable, variable); }
+#define DEBUG_MESSAGE(debug_message) { message(debug_message); }
+#define DEBUG_VALUE_OF(variable) { value_of(#variable, variable); }
+
+inline int indentation = 0;
+// Do not use these streams directly, use the wrapper!
+// At startup they can be undefined due to initialization order!
+// std::cout is only known at runtime!
+inline std::ostream* debug_stream = &std::cout;
+inline std::ofstream debug_file_stream;
+
+inline std::ostream* safe_output_stream() {
+    if (debug_stream != nullptr) {
+        return debug_stream;
+    }
+    return &std::cout;
+}
+
+inline void set_stream(std::ostream& stream) {
+	debug_stream = &stream;
+}
+
+template<class T> void value_of(const std::string& name, const T& value);
+
+inline void write_indentation() {
+	for(int i = 0; i < 2 * indentation; ++i) {
+        *safe_output_stream() << " ";
+	}
+}
 
 class CLog {
   public:
     CLog(const std::string& context);
     ~CLog();
-  public:
-    static void log_to_file();
-    void message(const std::string& message);
-    template<class T> void value_of(const std::string& name, const T& value);
-    static void set_stream(std::ostream& stream);
-  private:
-    void write_indentation();
-    std::ostream* safe_output_stream();
-  private:
-    static int indentation;
     const std::string context;
     clock_t start_time;
 };
 
-template<class T> void CLog::value_of(const std::string& name, const T& value) {
+template<class T> void value_of(const std::string& name, const T& value) {
     write_indentation();
     *safe_output_stream() << name;
     *safe_output_stream() << "=[" << value << "]" << std::endl;
     // endl causes a flush, so all extra flushing removed
 }
 
-inline int CLog::indentation = 0;
-// Do not use these streams directly, use the wrapper!
-// At startup they can be undefined due to initialization order!
-// std::cout is only known at runtime!
-inline std::ostream* debug_stream = &std::cout;
-inline std::ofstream debug_file_stream;
+inline void log_to_file() {
+    debug_file_stream.open("debug.txt");
+    set_stream(debug_file_stream);
+}
+
+inline void message(const std::string& message) {
+	write_indentation();
+	*safe_output_stream() << message << std::endl;
+}
 
 inline CLog::CLog(const std::string& ctx)	: context(ctx)
 #ifdef DEBUG_LOG_ENABLE_TIMING
@@ -79,13 +98,6 @@ inline CLog::CLog(const std::string& ctx)	: context(ctx)
 	++indentation;
 }
 
-inline std::ostream* CLog::safe_output_stream() {
-    if (debug_stream != nullptr) {
-        return debug_stream;
-    }
-    return &std::cout;
-}
-
 inline CLog::~CLog() {
 	--indentation;
 	write_indentation();
@@ -94,26 +106,6 @@ inline CLog::~CLog() {
 	*safe_output_stream() << " in " << ((double)(clock() - start_time) / CLOCKS_PER_SEC) << "s";
 #endif
 	*safe_output_stream() << std::endl;
-}
-
-inline void CLog::set_stream(std::ostream& stream) {
-	debug_stream = &stream;
-}
-
-inline void CLog::write_indentation() {
-	for(int i = 0; i < 2 * indentation; ++i) {
-        *safe_output_stream() << " ";
-	}
-}
-
-inline void CLog::log_to_file() {
-    debug_file_stream.open("debug.txt");
-    set_stream(debug_file_stream);
-}
-
-inline void CLog::message(const std::string& message) {
-	write_indentation();
-	*safe_output_stream() << message << std::endl;
 }
 
 #endif
