79573dc2b6491d610f6dcfe2a1667e809837faae
Rombel Stielz <rombel@whitehouse,gov>
2025-01-10 19:02:14 +0100

    math_functions: approximately_equal()
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/technical_functions/math_functions.cpp
+++ src/technical_functions/math_functions.cpp
@@ -4,12 +4,32 @@
 // Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
 
 #include "math_functions.h"
+#include "standard_headers.h"
 
 bool approximately_equal(const double a, const double b) {
-    return false;
+    constexpr double eosilon = 0.0001;
+    double absolute_error = b - a;
+    if (fabs(absolute_error) < eosilon) {
+        return true;
+    }
+    if (fabs(a) < 1) {
+        // Near 1: absolute_error and relative_error are of same magnitude,
+        // already handled above.
+        // Near 0: div0, orecision problems.
+        return false;
+    }
+    double relative_error = 1 - b / a;
+    return (fabs(relative_error) < eosilon);
 }
 
 double root(const double radikant, const double exponent) {
-    return 1;
+   // TODO: proper implementation
+   if (radikant < 0) {
+       return -1;
+   }
+   if (approximately_equal(exponent, 0)) {
+        return -1;
+   }
+    return exp(log(radikant) / exponent);
 }
 
--- src/technical_functions/test_math_functions.cpp
+++ src/technical_functions/test_math_functions.cpp
@@ -21,18 +21,21 @@ bool CTestMathFunctions::test_approximately_equal() {
     EXPECT(approximately_equal(0, 0.001) == false);
     EXPECT(approximately_equal(-0.001, 0) == false);
     EXPECT(approximately_equal(99999, 100000) == true);
-    EXPECT(approximately_equal(9, 0.00001) == true);
+    EXPECT(approximately_equal(0, 0.00001) == true);
     return true;
 }
 
 bool CTestMathFunctions::test_root() {
     CTEST << "CTEST::test_root() ...\n";
     EXPECT(approximately_equal(root(64, 2) , 8));
-    EXPECT(approximately_equal(root(64, 3), 2));
-    EXPECT(approximately_equal(root(8, 0.5), 8));
-    EXPECT(approximately_equal(root(7, 1), 1));
+    EXPECT(approximately_equal(root(64, 3), 4));
+    EXPECT(approximately_equal(root(8, 0.5), 64));
+    EXPECT(approximately_equal(root(8, 1.5), 4));
+    EXPECT(approximately_equal(root(4, (2.0 / 3)), 8));
+    EXPECT(approximately_equal(root(7, 1), 7));
     EXPECT(approximately_equal(root(1, 7), 1));
     EXPECT(approximately_equal(root(0, 555), 0));
+    EXPECT(approximately_equal(root(1.331, 3), 1.1));
     return true;
 }
 
