4f02dab0d7e7489e0db60ec532a54c495c487f25
Rombel Stielz <rombel@whitehouse,gov>
2025-01-25 17:44:44 +0100

    Integratiing (empty) PSV-tables
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/evaluator/evaluator.cpp
+++ src/evaluator/evaluator.cpp
@@ -4,9 +4,8 @@
 // Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
 
 #include "evaluator.h"
+#include "piece_square_value_tables.h"
 #include "../board/board.h"
-
-constexpr int half_pawn = 50;
 
 int CEvaluator::evaluate() const {
     int score = 0;
@@ -23,11 +22,17 @@ int CEvaluator::evaluate_square(const SSquare &square) const {
 }
 
 int CEvaluator::evaluate_square(const int file, const int rank) const {
-    return 3141;
+    assert(file_in_range(file));
+    assert(rank_in_range(rank));
+    char square_content = board.get_square(file, rank);
+    assert(square_content <= LAST_PIECE_TYPE);
+    int result = main_piece_square_value_table_set[square_content][file][rank];
+    return result;
 }
 
 bool CEvaluator::evaluates_approximately_to(const int score) const {
     int real_score = evaluate();
+    constexpr int half_pawn = 50;
     return (abs(real_score - score) < half_pawn);
 }
 
