4199dd5dff6edfc83ed409dc1ca340920a0321e5
Rombel Stielz <rombel@whitehouse,gov>
2025-02-07 08:20:03 +0100

    Testcases, some failed or freeze
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/move_generator/test_perft.cpp
+++ src/move_generator/test_perft.cpp
@@ -21,8 +21,12 @@ typedef struct {
 typedef std::vector<STestcase> VTestcaseCollection;
 
 const VTestcaseCollection testcase_collection = {
-    { "startpos", { 1, 20, 400, 8902, 197281, 4865609, 119060324 } },
-    { "", { 1, 0, 0, 0, 0, 0, 0 } },
+ //+   { "startpos", { 1, 20, 400, 8902, 197281, 4865609, 119060324 }},
+////-    { "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ", { 1, 48, 2039, 97862, 4085603, 193690690, 8031647685 }},
+//+    { "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ", { 1, 14, 191, 2812,  43238, 674624, 11030083 }},
+////-    { "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", { 1, 6, 264, 9467, 422333, 15833292, 706045033 }},
+////-    { "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ", { 1, 44, 1486, 62379, 2103487, 89941194, 0 }},
+    { "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ", { 1, 46, 2079, 89890, 3894594, 164075551, 6923051137 }},
 };
 
 bool CTestPerft::test_everything() {
@@ -33,7 +37,7 @@ bool CTestPerft::test_everything() {
 
 bool CTestPerft::test_shallow_depth() {
     TEST_FUNCTION();
-    constexpr unsigned int shallow_depth = 3;
+    constexpr unsigned int shallow_depth =6 ;
     EXPECT(test_up_to_depth(shallow_depth));
     return true;
 }
@@ -50,13 +54,14 @@ bool CTestPerft::test_up_to_depth(const unsigned int depth) {
             CTEST << "Depth: " << j << "\n";
             CTEST << "Expected: " <<expeted_result << "\n";
             int64_t true_result = perft(testcase.fen_position, j);
+            CTEST << "Result: " << true_result << "\n";
             SILENT_EXPECT(expeted_result == true_result);
         }
    }
    return true;
 }
 
-int64_t CTestPerft::perft( const std::string &fen_position, const unsigned int depth) {
+int64_t CTestPerft::perft(const std::string &fen_position, const unsigned int depth) {
      SILENT_EXPECT(board.set_fen_position(fen_position));
      return perft(depth);
 }
