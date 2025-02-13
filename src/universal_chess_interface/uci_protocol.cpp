443cb5fd8543fbab126628bbc7ff283a1dec90dc
Rombel Stielz <rombel@whitehouse,gov>
2025-02-13 19:13:30 +0100

    version_0.b
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/universal_chess_interface/uci_protocol.cpp
+++ src/universal_chess_interface/uci_protocol.cpp
@@ -11,7 +11,7 @@
 #include "../technical_functions/engine_test.h"
 #include "../technical_functions/standard_headers.h"
 
-const std::string ENGINE_ID = "Schachfeld_Engine_0.a";
+const std::string ENGINE_ID = "Schachfeld_Engine_0.b";
 static_assert('a' > '9');
 
 CUciProtocol::CUciProtocol() {
