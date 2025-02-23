610bc3296d49f64cffd1441879d98e1ca4135370
Rombel Stielz <rombel@whitehouse,gov>
2025-02-23 17:09:02 +0100

    PSV: french advance structure
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/evaluator/test_evaluator.cpp
+++ src/evaluator/test_evaluator.cpp
@@ -33,6 +33,9 @@ const std::vector<STestcaseEvaluator> testcases_evaluator = {
     // Central pawn structure: c4 + d4 / d5 + e6
     { "8/8/2p/3p/2PP w", "8/8/8/3p/3P w" },
     { "8/8/2p/8/3P w", "8/8/2p/3p/2PP w" },
+    // Central pawn structure: c4 + d4 + c3 / c5 + d5 + e6
+    { "8/8/4p/2ppP/3P/2P w", "8/8/4p/3pP/8/2P w" },
+    { "8/8/4p/3pP/3P w", "8/8/4p/2ppP/3P/2P w" },
     // Pawns on 5th / 6th / 7th rank: semi-strong / monsters / potentially a bit weak
     { "8/8/PPPPPPPP w", "8/8/8/PPPPPPPP w" },
     { "8/8/PPPPPPPP w", "8/PPPPPPPP w" },
