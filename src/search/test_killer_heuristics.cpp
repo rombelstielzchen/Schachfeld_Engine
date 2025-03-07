fb5eaa1750c466871aa1ec9cd8f11f45d92d2208
Rombel Stielz <rombel@whitehouse,gov>
2025-03-07 22:29:25 +0100

    tests for killers, not even red
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

---
+++ src/search/test_killer_heuristics.cpp
@@ -0,0 +1,13 @@
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+#include "test_killer_heuristics.h"
+#include "../technical_functions/testing.h"
+
+bool CTestKillerHeuristic::test_everything() {
+    BEGIN_TESTSUITE("CTestKillerHeuristic");
+    return false;
+}
+
---
+++ src/search/test_killer_heuristics.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+#include "../move_generator/move.h"
+#include "../technical_functions/standard_headers.h"
+
+class CTestKillerHeuristic {
+   public:
+    static bool test_everything(); 
+};
+
