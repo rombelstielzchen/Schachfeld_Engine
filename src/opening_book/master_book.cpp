5714444900f08df165bdbbb99ce7c5029491a6dd
Rombel Stielz <rombel@whitehouse,gov>
2025-03-22 20:09:46 +0100

    Using book-options, except solid_mix
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/opening_book/master_book.cpp
+++ src/opening_book/master_book.cpp
@@ -19,7 +19,22 @@ CMasterBook::CMasterBook() : gm_book(sorted_variation_collection_gm_book),
 }
 
 std::string CMasterBook::get_move(const std::string &moves_from_startpos_in_uci_format) {
-        std::string book_move = tabijas.get_move(moves_from_startpos_in_uci_format);
+    std::string book_move = NULL_MOVE_AS_TEXT;
+    switch (boook_option) {
+        case BOOK_OPTIONS_BROAD_GM:
+           // Nothing toi be done here in order to avoid double lookups.
+           // Broad_GM is our backup for failed lookups.
+            break;
+        case BOOK_OPTIONS_TABIJAS:
+            book_move = tabijas.get_move(moves_from_startpos_in_uci_format);
+            break;
+        case BOOK_OPTIONS_WONDER_WEAPONS:
+            // TODO
+            break;
+        case BOOK_OPTIONS_SOLID_MIX:
+            // TODO
+            break;
+    }
     if (book_move == NULL_MOVE_AS_TEXT) {
        book_move = gm_book.get_move(moves_from_startpos_in_uci_format);
     }
@@ -43,6 +58,5 @@ void CMasterBook::set_option(const std::string &selected_uci_option) {
        // Ignore unexpected vales from a potential stupid / malicious outside world 
     }
     std::cerr << "boook_option: " << boook_option << "\n";
-
 }
 
