dc322917ba933a59af2873c78d8eb8ed36c06ff2
Rombel Stielz <rombel@whitehouse,gov>
2025-04-05 07:39:05 +0200

    tinkering
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- test/quick_engine_test.pl
+++ test/quick_engine_test.pl
@@ -12,60 +12,29 @@
 # but propably will do if you redefine ENGINE below and use a shell like Git-Bash.
 
 use strict;
-use warnings;
+#use warnings;
+use IPC::Open2;
 
 use constant ENGINE => "../src/engine";
 use constant EXIT_SUCCESS => 0;
 use constant EXIT_FAILURE => 1;
-use constant MODE_INPUT_PIPE => "-|";
-use constant MODE_OUTPUT_PIPE => "|-";
 
 my $input_pipe;
 my $output_pipe;
 
-sub EXPECT {
-    my ($condition) = @_;
-    if (!$condition) {
-        die "[ERROR] condition failed\n";
-    }
-}
+print "Going to start engin: ", ENGINE, "\n";
+my $pid = open2($input_pipe, $output_pipe, ENGINE)
+    or die "open2() failed $!";
+print "PID: ", $pid, "\n";
 
-sub send_message {
-    my ($message) = @_;
-    print ">>> ", $message;
-    print $output_pipe $message;
-}
+sleep 25;
+print $output_pipe "isready\n";
+#flush $output_pipe;
+print "dadasd";
+my $date = <input_pipe>;
+print $pid, " huhu\n";
+print "--- ", $date, " huhuhu\n";
+#exit EXIT_SUCCESS;
 
-### BOMP -- Begin Of Main Program ######
-print "### Quick Engine Test ########\n";
-print "Starting engine: @{[ ENGINE ]}\n";
-my $pid = fork();
-die "Failed to foek: $!" unless defined $pid;
-if ($pid == 0) {
-    exit;
-    exec(ENGINE);
-    # exec replaces the process and never returns.
-    # No further script actions in this case
-}
-die "Unexpected PID after fork" if $pid == 0;
-our $child_pid = $pid;
-print "Child PID: $child_pid\n";
-print "Setting up pipes for communication ...\n";
-open $output_pipe, MODE_OUTPUT_PIPE, @{[ ENGINE ]} or die $!;
-#open input_pipe, MODE_INPUT_PIPE, @{[ ENGINE ]} or die $!;
-send_message("uci\n");
-send_message("isready\n");
-send_message("quit\n");
-exit(EXIT_SUCCESS);
-
-
-#sub await_inout { 
-#    my ($expected_input, $timeout_seconds) = @_;
-#    my $input = "";
-#    while ($input neq "readyok") {
-#        $input = <>;
-#    }
-#    print "gugu";
-    #}
 
 
