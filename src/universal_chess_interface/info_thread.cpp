b0ce5772433d437ec6d6451d083c52db36779987
Rombel Stielz <rombel@whitehouse,gov>
2025-06-08 19:14:25 +0200

    info_thread: empty files + makefile
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

---
+++ src/universal_chess_interface/info_thread.cpp
@@ -0,0 +1,13 @@
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+#include "info_thread.h"
+
+CInfoThread::CInfoThread() {
+}
+
+void CInfoThread::thread_function() {
+}
+
---
+++ src/universal_chess_interface/info_thread.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+class CInfoThread {
+  public:
+    CInfoThread();
+  private:
+    void thread_function();
+};
+
