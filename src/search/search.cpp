cdacdf6588843d58e0d3356925d70b5131b779fd
Rombel Stielz <rombel@whitehouse,gov>
2025-06-12 20:08:34 +0200

    code-simplification
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -21,9 +21,9 @@ inline int CSearch::losing_score(bool losing_side) {
 
 int CSearch::alpha_beta(int remaining_depth, int distance_to_root, SAlphaBetaWindow alpha_beta_window) {
     assert(remaining_depth >= 0);
-    // TODO: Revisit this, related to stalemate-detection
+    assert(distance_to_root > 0);
     assert(is_valid_alpha_beta_window(alpha_beta_window)); 
-///    int score = board.evaluator.evaluate();
+    // TODO: Revisit this, related to stalemate-detection
     if (remaining_depth <= 0) {
         return quiescence(QUIESCENCE_DEPTH, distance_to_root, alpha_beta_window);
     }
@@ -38,7 +38,6 @@ int CSearch::alpha_beta(int remaining_depth, int distance_to_root, SAlphaBetaWin
     // TODO: replace bad stalemate-logic below
     if ((distance_to_root == 1) && no_legal_moves()) {
         if (CBoardLogic::piece_attacked_by_side_not_to_move(CBoardLogic::king_square(side_to_move)) == false) {
-            std::cerr << "not in check, draw\n";
             return SCORE_DRAW;
         } else {
             return losing_score(side_to_move);
@@ -46,14 +45,10 @@ int CSearch::alpha_beta(int remaining_depth, int distance_to_root, SAlphaBetaWin
     }
     // TODO: replace bad stalemate-logic above
     int n_moves = move_generator.move_list.list_size();
-///    if (n_moves <= 0) {
-///        best_score = board.evaluator.evaluate();
-///        // TODO: fluctuations of best_score ruin the logic
-///        best_score += (side_to_move == WHITE_PLAYER) ? - remaining_depth : remaining_depth;
-///        return best_score;
-///    }
     for (int j = 0; j < n_moves; ++j) {
         SMove move_candidate = move_generator.move_list.get_next__capture_killer_silent(distance_to_root);
+        assert(is_null_move(move_candidate) == false);
+        assert(move_in_range(move_candidate));
         board.move_maker.make_move(move_candidate);
         int candidate_score;
         if (remaining_depth > 1) {
@@ -103,16 +98,14 @@ int CSearch::quiescence(int remaining_depth, int distance_to_root, SAlphaBetaWin
     CMoveGenerator move_generator;
     move_generator.generate_captures();
     int n_moves = move_generator.move_list.list_size();
-    if (n_moves <= 0) {
-        // position is quiet
-        return best_score;
-    }
     for (int j = 0; j < n_moves; ++j) {
         SMove move_candidate = move_generator.move_list.get_next__best_capture();
-        assert(move_candidate.move_type != MOVE_TYPE_NORMAL);
         assert(is_null_move(move_candidate) == false);
+        assert(move_in_range(move_candidate));
+        assert(is_any_capture(move_candidate));
         board.move_maker.make_move(move_candidate);
         int candidate_score;
+        // TODO: > 0?
         if (remaining_depth > 1) {
             candidate_score = quiescence(remaining_depth - 1, distance_to_root + 1, alpha_beta_window);
         } else {
