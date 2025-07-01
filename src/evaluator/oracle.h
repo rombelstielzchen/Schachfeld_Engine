1c0ef68eea9155de2edc8c242fd0a9966602e814
Rombel Stielz <rombel@whitehouse,gov>
2025-07-01 21:49:35 +0200

    empty oracle + makefile
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

---
+++ src/evaluator/oracle.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Project: Schachfeld_Engine
+// Author: Rombelstielzchen
+// License: GPLv3
+// Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
+
+#include "oracle.h"
+
+COracle::C::COracle() {
+}
+
+void COracle::configure_knowledge() {
+}
+
