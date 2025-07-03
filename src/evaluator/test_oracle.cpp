282a1e4c1d7fee2e265dc517d687ed21b3b63682
Rombel Stielz <rombel@whitehouse,gov>
2025-07-03 13:24:09 +0200

    test_oracle: 1 more test, green
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/evaluator/test_oracle.cpp
+++ src/evaluator/test_oracle.cpp
@@ -4,6 +4,7 @@
 // Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
 
 #include "test_oracle.h"
+#include "test_evaluator.h"
 #include "piece_square_value_tables.h"
 #include "../board/board.h"
 #include "../technical_functions/testing.h"
@@ -39,6 +40,9 @@ bool CTestOracle::test_reasonable_initialization() {
 
 bool CTestOracle::test_endgame_king() {
     TEST_FUNCTION();
+    std::string middlegame_Kg1 = "4k2r/2p1p2r/1pPpPp1r/pP1P1Pp1/P5Pp/R6P/R7/R5K1 w - - 0 1";
+    std::string middlegame_Ke3 = "4k2r/2p1p2r/1pPpPp1r/pP1P1Pp1/P5Pp/R3K2P/R7/R7 w - - 0 1";
+    EXPECT(first_position_better(middlegame_Kg1, middlegame_Ke3));
     return true;
 }
 
