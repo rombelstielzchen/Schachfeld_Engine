2e796d160a97ec2e9434f087d68b9cbff111187b
Rombel Stielz <rombel@whitehouse,gov>
2025-07-20 10:30:01 +0200

    moved struct SAlphaBetaWindow
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/search/iterative_deepening.h
+++ src/search/iterative_deepening.h
@@ -8,6 +8,38 @@
 #include"../move_generator/move.h"
 #include"../move_generator/move_generator.h"
 
+// TODO: use constants from evaluator.h
+// TODO: stalemate, proper distance to mate
+// We need some range here in order to distinguish different depths to mate,
+// avoiding nonsense moves to longer nates or worse
+//
+// Caveat! INT32_MIN and INT32_MAX are not symmetric.
+// Naively used, this causes an overflow when doing negamax.
+constexpr int32_t BLACK_MIN_SCORE = INT32_MAX;
+constexpr int32_t WHITE_MIN_SCORE = -BLACK_MIN_SCORE;
+static_assert(-WHITE_MIN_SCORE > 0);
+static_assert(-BLACK_MIN_SCORE < 0);
+
+#pragma pack(push)
+#pragma pack(1)
+
+typedef struct {
+     int32_t alpha;
+     int32_t beta;
+} SAlphaBetaWindow;
+
+static_assert(sizeof(SAlphaBetaWindow) <= sizeof(int64_t));
+
+#pragma pack(pop)
+
+constexpr bool is_valid_alpha_beta_window(const SAlphaBetaWindow alpha_beta_window) {
+    return alpha_beta_window.alpha <= alpha_beta_window.beta;
+}
+
+constexpr SAlphaBetaWindow INFINITE_ALPHA_BETA_WINDOW = { WHITE_MIN_SCORE, BLACK_MIN_SCORE };
+
+static_assert(is_valid_alpha_beta_window(INFINITE_ALPHA_BETA_WINDOW));
+
 constexpr int64_t INFINITE_DEPTH = INT_MAX;
 
 class CIterativeDeepening {
