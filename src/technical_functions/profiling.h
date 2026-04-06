e92c89e33e139a7429a0cdd0cc5723b155b81332
Rombel Stielz <rombel@whitehouse,gov>
2026-04-06 16:02:10 +0200

    New module: profiling
    Signed-off-by: Rombel Stielz <rombel@whitehouse,gov>

--- src/technical_functions/profiling.h
+++ src/technical_functions/profiling.h
@@ -6,17 +6,22 @@
 // Forum: https://www.schachfeld.de/threads/40956-einen-namen-fuer-das-baby
 
 #include <string>
+#include <vector>
 
 class CProfiling {
   public:
     CProfiling();
   public:
     void reset();
-    void increment(int index_of_counter);
-    void increment_if(int index_of_counter, bool condition);
+    void increment(size_t index_of_counter);
+    void increment_if(size_t index_of_counter, bool condition);
   public:
     void show_results() const;
-    void show_result(int index_of_counter, std::string description) const;
+    void show_result(size_t index_of_counter, const std::string &description) const;
+  private:
+    void size_up(size_t n_entries);
+  private:
+    std::vector<uint64_t> counters;
 };
 
 inline CProfiling profiling;
